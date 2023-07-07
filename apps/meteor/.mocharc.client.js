66f3d48c17f3b3a4eed7701872b7cf2e4500bc85
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-07-07 12:18:30 -0300

    ..

--- apps/meteor/.mocharc.client.js
+++ apps/meteor/.mocharc.client.js
@@ -38,5 +38,5 @@ module.exports = {
 		'tests/unit/lib/**/*.tests.ts',
 		'tests/unit/client/**/*.test.ts',
 	],
-	exclude: ['client/hooks/*.spec.{ts,tsx}'],
+	exclude: ['client/hooks/**/*.spec.{ts,tsx}', 'client/components/**/*.spec.{ts,tsx}'],
 };
--- apps/meteor/jest.client.config.ts
+++ apps/meteor/jest.client.config.ts
@@ -3,7 +3,7 @@ export default {
 
 	testEnvironment: 'jsdom',
 	modulePathIgnorePatterns: ['<rootDir>/dist/'],
-	testMatch: ['<rootDir>/client/hooks/**.spec.[jt]s?(x)', '<rootDir>/client/components/**.spec.tsx', '/client/hooks/**.spec.ts'],
+	testMatch: ['<rootDir>/client/hooks/**/**.spec.[jt]s?(x)', '<rootDir>/client/components/**/**.spec.tsx'],
 	transform: {
 		'^.+\\.(t|j)sx?$': '@swc/jest',
 	},
