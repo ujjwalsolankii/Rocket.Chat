996f569fa0ad46e465450ef5387924b8ed7dc03c
Guilherme Gazzo <guilhermegazzo@gmail.com>
2023-07-07 10:43:54 -0300

    creates feature preview component

---
+++ apps/meteor/client/components/FeaturePreview.spec.tsx
@@ -0,0 +1,62 @@
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import React from 'react';
+
+import { MockedSettingsContext, MockedUserContext } from '../hooks/useFeaturePreview.spec';
+import { FeaturePreview, FeaturePreviewOff, FeaturePreviewOn } from './FeaturePreview';
+
+test('should renders off if the feature is disabled', async () => {
+	render(
+		<FeaturePreview feature='quickReactions'>
+			<FeaturePreviewOn>on</FeaturePreviewOn>
+			<FeaturePreviewOff>off</FeaturePreviewOff>
+		</FeaturePreview>,
+		{
+			wrapper: ({ children }) => (
+				<MockedSettingsContext
+					settings={{
+						Accounts_AllowFeaturePreview: true,
+					}}
+				>
+					<MockedUserContext
+						userPreferences={{
+							featuresPreview: [],
+						}}
+					>
+						{children}
+					</MockedUserContext>
+				</MockedSettingsContext>
+			),
+		},
+	);
+
+	expect(screen.getByText('off')).toBeInTheDocument();
+});
+
+test('should renders on if the feature is enabled', async () => {
+	render(
+		<FeaturePreview feature='quickReactions'>
+			<FeaturePreviewOn>on</FeaturePreviewOn>
+			<FeaturePreviewOff>off</FeaturePreviewOff>
+		</FeaturePreview>,
+		{
+			wrapper: ({ children }) => (
+				<MockedSettingsContext
+					settings={{
+						Accounts_AllowFeaturePreview: true,
+					}}
+				>
+					<MockedUserContext
+						userPreferences={{
+							featuresPreview: [{ name: 'quickReactions', value: true }],
+						}}
+					>
+						{children}
+					</MockedUserContext>
+				</MockedSettingsContext>
+			),
+		},
+	);
+
+	expect(screen.getByText('on')).toBeInTheDocument();
+});
